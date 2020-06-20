f71c29f8f04b5522d46a3641c27bb70595fdd2f9
Русских Евгения <russkih.evgeniya92@yandex.ru>
2020-06-20 10:24:39 +0300

    add template

--- frontend/src/plugins/theme.js
+++ frontend/src/plugins/theme.js
@@ -1,11 +1,8 @@
-import colors from 'vuetify/lib/util/colors'
-
 export default {
   primary: {
-    base: colors.cyan.base,
-    darken1: colors.cyan.darken1,
-    darken2: colors.cyan.darken2,
-    darken3: colors.cyan.darken3,
-    darken4: colors.cyan.darken4,
+    base: '#266dc2',
+    darken1: '#343d57',
+    darken2: '#262c40',
+    lighten1: '#ffffff'
   }
 }
